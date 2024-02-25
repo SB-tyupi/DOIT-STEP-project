941ebc3a86ee6ffa0128613ef3841aabc832c0c6
SB_twin <88552610+SB-tyupi@users.noreply.github.com>
2024-02-25 15:10:15 +0400

    Hangman game

--- Program.cs
+++ Program.cs
@@ -1,136 +1,150 @@
-﻿using System.Text;
-
-namespace DOIT_Project1
+﻿namespace DOIT_Project2
 {
     /*
-     Georgian Letter Translator
-
-    translates english word to georgian words
+     Game: Hangman
+    program shall choose random word predifined in list
+    and display underscores amount of letters in the secret word.
+    user shall guess the letter and if the letter is in the secret word,
+    it shall reveal the the letter in correct place.
     */
     internal class Program
     {
+        public static void visual(int guess)
+        {
+            switch (guess)
+            {
+                case 1:
+                    Console.WriteLine(" _______\r\n |     \\\r\n |     () \r\n |\r\n |\r\n |_______\r\n");
+                    break;
+                    case 2:
+                    Console.WriteLine(" _______\r\n |     \\\r\n |     () \r\n |     ||\r\n |\r\n |_______\r\n");
+                    break;
+                    case 3:
+                    Console.WriteLine(" _______\r\n |     \\\r\n |     () \r\n |    /||\r\n |\r\n |_______\r\n");
+                    break;
+                    case 4:
+                    Console.WriteLine(" _______\r\n |     \\\r\n |     () \r\n |    /||\\\r\n |\r\n |_______\r\n");
+                    break;
+                    case 5:
+                    Console.WriteLine(" _______\r\n |     \\\r\n |     () \r\n |    /||\\\r\n |     /\r\n |_______\r\n");
+                    break;
+                    case 6:
+                    Console.WriteLine(" _______\r\n |     \\\r\n |     () \r\n |    /||\\\r\n |     /\\\r\n |_______\r\n");
+                    break;
+            }
+        }
+
+        //public static void checkChar(char ch, char[] word, string str)
+        //{
+        //    int count = 0;
+        //    //adds letters in correct spot
+        //    for (int i = 0; i < str.Length; i++)
+        //    {
+        //        if(ch == str[i])
+        //        {
+        //            word[i] = ch;
+        //            count++;
+        //        }
+        //        if (count == str.Length)
+        //        {
+        //            Console.WriteLine("Congrats! You have won the game!");
+        //        }
+        //    }
+        //    for (int i = 0;i < str.Length;i++)
+        //    {
+        //        Console.Write(word[i]);
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        
         static void Main(string[] args)
         {
-            string input;
-            Console.WriteLine("Enter the text and this program will tranform your words into Georigian alphabet!");
-            Console.OutputEncoding = Encoding.UTF8;
-            input = Console.ReadLine();
+            Console.WriteLine("This is the game HANGMAN! " +
+                "\nProgram will choose random word and you have to guess letters!");
+            List<string> word = new List<string> {"money", "car", "computer", "step", "phone",
+                "program", "air", "factory", "positive" };
 
-            char[] text = new char[input.Length];
+            Random random = new Random();
 
-            for (int i = 0; i < text.Length; i++)
+            int rand = random.Next(word.Count);
+
+            string secret_word = word[rand];
+            char[] sWord_arr = new char[secret_word.Length];
+            //fills array with underscores
+            for (int i = 0; i < secret_word.Length; i++)
             {
-                switch (input[i])
-                {
-                    case 'a':
-                        text[i] = 'ა';
-                        break;
-                    case 'b':
-                        text[i] = 'ბ';
-                        break;
-                    case 'c':
-                        text[i] = 'ც';
-                        break;
-                    case 'C':
-                        text[i] = 'ჩ';
-                        break;
-                    case 'd':
-                        text[i] = 'დ';
-                        break;
-                    case 'e':
-                        text[i] = 'ე';
-                        break;
-                    case 'f':
-                        text[i] = 'ფ';
-                        break;
-                    case 'g':
-                        text[i] = 'გ';
-                        break;
-                    case 'h':
-                        text[i] = 'ჰ';
-                        break;
-                    case 'i':
-                        text[i] = 'ი';
-                        break;
-                    case 'j':
-                        text[i] = 'ჯ';
-                        break;
-                    case 'J':
-                        text[i] = 'ჟ';
-                        break;
-                    case 'k':
-                        text[i] = 'კ';
-                        break;
-                    case 'l':
-                        text[i] = 'ლ';
-                        break;
-                    case 'm':
-                        text[i] = 'მ';
-                        break;
-                    case 'n':
-                        text[i] = 'ნ';
-                        break;
-                    case 'o':
-                        text[i] = 'ო';
-                        break;
-                    case 'p':
-                        text[i] = 'პ';
-                        break;
-                    case 'q':
-                        text[i] = 'ქ';
-                        break;
-                    case 'r':
-                        text[i] = 'რ';
-                        break;
-                    case 'R':
-                        text[i] = 'ღ';
-                        break;
-                    case 's':
-                        text[i] = 'ს';
-                        break;
-                    case 'S':
-                        text[i] = 'შ';
-                        break;
-                    case 't':
-                        text[i] = 'ტ';
-                        break;
-                    case 'T':
-                        text[i] = 'თ';
-                        break;
-                    case 'u':
-                        text[i] = 'უ';
-                        break;
-                    case 'v':
-                        text[i] = 'ვ';
-                        break;
-                    case 'w':
-                        text[i] = 'წ';
-                        break;
-                    case 'W':
-                        text[i] = 'ჭ';
-                        break;
-                    case 'x':
-                        text[i] = 'ხ';
-                        break;
-                    case 'y':
-                        text[i] = 'ყ';
-                        break;
-                    case 'z':
-                        text[i] = 'ზ';
-                        break;
-                    case 'Z':
-                        text[i] = 'ძ';
-                        break;
-                    case ' ':
-                        text[i] = ' ';
-                        break;
-                    default:
-                        text[i] = '?';
-                        break;
-
-                }
-                Console.Write(text[i]);
+                sWord_arr[i] = '_';
             }
+
+            Console.WriteLine("Try not to hang the stickman like this!!");
+            visual(6);
+
+
+
+            int count = 0;
+            int wrong_guess = 0;
+            int count_guess = 0;
+            try { 
+            while (count != secret_word.Length)
+            {
+                char guess = char.Parse(Console.ReadLine());
+            if (guess == 0)
+            {
+                break;
+            }
+                
+                //adds letters in correct spot
+                for (int i = 0; i < secret_word.Length; i++)
+                {
+                    if (guess == secret_word[i])
+                    {
+                        sWord_arr[i] = guess;
+                        count++;
+                        count_guess++;
+                    }
+                }
+                
+                //visual part of the program (the stickman)
+                if (count_guess == 0)
+                {
+                    wrong_guess++;
+                    visual(wrong_guess);
+                    if (wrong_guess == 6)
+                    {
+                        Console.WriteLine("You have lost the game :((");
+                        break;
+                    }
+                }
+                else
+                {
+                    visual(wrong_guess);
+                    count_guess = 0;
+                }
+
+                //prints out the secret word
+                for (int i = 0; i < secret_word.Length; i++)
+                {
+                    Console.Write(sWord_arr[i]);
+                }
+                Console.WriteLine();
+
+                if (count == secret_word.Length)
+                {
+                    Console.WriteLine("Congrats! You have won the game!");
+                }
+
+            }
+
+            }catch (Exception e)
+            {
+                Console.WriteLine("Error happend: " + e.Message);
+            }
+
+
+
+
+
         }
     }
 }
